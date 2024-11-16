1204f72b70809023f74566353a8defbf0af393b2
aryfareq <121553546+aryfareq@users.noreply.github.com>
2024-11-16 21:16:02 +0300

    Main Class
    We now get the input from the user, and split them and put hem into an array, ready to be sent

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ import java.net.*;
 public class Main{
     public static void main(String[] args) {
         
+        //Getting the user text
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input the text you want to send: ");
+        String userInput = input.nextLine();
+
+        //Splitting the text
+        String[] textArr = userInput.split(" ");
+
+
     }
 }
